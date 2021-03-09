cba7f65bb17683233c7f3f671ac1569de3bbb58d
Davide Andreana <davin874@gmail.com>
2021-03-09 10:45:38 +0100

    Cart component e checkout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
 
 const routes: Routes = [
   { path: '', component: ProductListComponent },
   // products/:productId -> il ':' serve per indicare che qualsiasi ...
   // ... valore inserirò, lo metterà come productId
-  { path: 'products/:productId', component: ProductDetailsComponent }
+  { path: 'products/:productId', component: ProductDetailsComponent },
+  { path: 'cart', component: CartComponent }
 ];
 
 @NgModule({
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -10,6 +10,16 @@ export class CartService {
 
   addToCart(product) {
     this.items.push(product);
+    // log della variabile items di questa classe
+    console.log(this.items);
+
+    // calcoliamo il totale dei prezzi degli articoli comprati
+    let total = 0;
+
+    this.items.forEach(item => total+= item.price);
+    console.log('Costo totale: ' + total);
+
+
   }
 
   getItems() {
